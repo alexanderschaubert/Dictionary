ca10978a03f8f049c7cbc966bb23830077eb7b3c
Alexander Schaubert <alexander.schaubert@googlemail.com>
2020-09-16 21:01:27 +0200

    init commit

--- complexDirectory.py
+++
@@ -1,8 +0,0 @@
-complex_1 = {1: {'Name':'Jack'},
-             2: {'Gender':'M'}}
-print(complex_1.keys())
-
-complex_2 = {'Dict1': {'Name': 'someName'},
-             'Dict2': {'info':'some Information'}}
-print(complex_2.keys())
-print(complex_2['Dict1']['Name'])
